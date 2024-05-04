d971ed9686075ad1088a529245cebf38e7da29bc
Eloise2011 <joshuahbrooks@163.com>
2024-05-04 23:17:17 +0800

    Initial commit - Thymeleaf Learning， with Spring Boot. Env has been set up successfully.

---
+++ src/main/java/elo/pra/App.java
@@ -0,0 +1,19 @@
+package elo.pra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description First Demo of SpringBoot Thymeleaf
+ * @date 2024-05-04 22:07
+ * @author Joshua.H.Brooks 
+ */
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        // TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        SpringApplication.run(App.class,args);
+    }
+}
---
+++ src/main/java/elo/pra/controller/HelloController.java
@@ -0,0 +1,27 @@
+package elo.pra.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Joshua.H.Brooks
+ * @description
+ * @date 2024-05-04 22:55
+ */
+@Controller
+public class HelloController {
+    @RequestMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("name","乔叔华");
+        return "index";
+    }
+
+    @RequestMapping("hi")
+    public String hi(Model model, HttpServletRequest request) {
+        request.setAttribute("name","布鲁克斯");
+        return "index";
+    }
+}
